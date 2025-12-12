1905b6dd9c5e89765f8b0afef14213c3d50d0c64
Armanak4786 <arman.khan@aurionpro.com>
2025-12-12 11:36:08 +0530

    create module and componnet for sanket

---
+++ projects/modules/reimbursement-details/reimbursement-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reimbursement-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './reimbursement-details.component.html',
+  styleUrl: './reimbursement-details.component.scss'
+})
+export class ReimbursementDetailsComponent {
+
+}
---
+++ projects/modules/reimbursement-details/reimbursement-details.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReimbursementDetailsRoutingModule } from './reimbursement-details-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReimbursementDetailsRoutingModule
+  ]
+})
+export class ReimbursementDetailsModule { }
